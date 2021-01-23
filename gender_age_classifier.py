b63df00ae253c2ec60db108bb5eab9254919cbaf
Park chanyeong <qkrcks077@gmail.com>
2021-01-23 12:18:43 +0900

    face detect

--- gender_age_classifier.py
+++ gender_age_classifier.py
@@ -18,17 +18,54 @@ gender_classifier_config = "models/deploy_gender.prototxt"
 age_classifier_model = "models/age_net.caffemodel"
 age_classifier_config = "models/deploy_age.prototxt"
 
+face_detect_net = cv2.dnn.readNet(face_detect_model, face_detect_config)
+gender_classifier_net = cv2.dnn.readNet(gender_classifier_model, gender_classifier_config)
+age_classifier_net = cv2.dnn.readNet(age_classifier_model, age_classifier_config)
+
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Camera open faile!")
     sys.exit()
 
-face_detect_net = cv2.dnn.readNet(face_detect_model, face_detect_config)
-gender_classifier_net = cv2.dnn.readNet(gender_classifier_model, gender_classifier_config)
-age_classifier_net = cv2.dnn.readNet(age_classifier_model, age_classifier_config)
+while True:
+    ret, frame = cap.read()
 
+    if not ret:
+        break
 
+    blob1 = cv2.dnn.blobFromImage(frame, scalefactor=1, size=(300, 300),\
+                                  mean=(104, 177, 123))
+    net.setInput(blob1)
+    out = net.forward()
+
+    face_detected = out[0, 0, :, :]
+    (h,w) = frame.shape[:2]
+
+    for i in range(face_detected.shape[0]):
+        confidence = face_detected[i, 2]
+        if confidence < 0.5:
+            break
+
+        x1 = int(face_detected[i, 3] * w)
+        y1 = int(face_detected[i, 4] * h)
+        x2 = int(face_detected[i, 5] * w)
+        y2 = int(face_detected[i, 6] * h)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0))
+
+        label = f'Face: {confidence:4.2f}'
+        cv2.putText(frame, label, (x1, y1 - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+
+    blob2 = cv2.dnn.blobFromImage(frame, scalefactor=1, size=(227,227), \
+            mean=(74.4263377603, 87.7689143744, 114.895847746), \
+            swapRB=False, crop=False)
+    
+    # gender_classifier_net.setInput(blob)
+    # gender_out = gender_classifier_net.forward()
+    # gender = 123
 
 
 

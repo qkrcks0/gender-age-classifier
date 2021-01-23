61bccf25e94c3cfa6a194485117f874bbb32d40c
Park chanyeong <qkrcks077@gmail.com>
2021-01-23 17:26:21 +0900

    create image folder

--- gender_age_classifier.py
+++ gender_age_classifier.py
@@ -19,13 +19,15 @@ gender_classifier_config = "models/deploy_gender.prototxt"
 age_classifier_model = "models/age_net.caffemodel"
 age_classifier_config = "models/deploy_age.prototxt"
 
+# create net
 face_detect_net = cv2.dnn.readNet(face_detect_model, face_detect_config)
 gender_classifier_net = cv2.dnn.readNet(gender_classifier_model, gender_classifier_config)
 age_classifier_net = cv2.dnn.readNet(age_classifier_model, age_classifier_config)
 
-imgs_list = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg",\
-             "07.jpg","08.jpg","09.jpg","10.jpg","11.jpg","12.jpg",\
-             "13.jpg","14.jpg","15.jpg"]
+# image list
+imgs_list = ["imgs/01.jpg","imgs/02.jpg","imgs/03.jpg","imgs/04.jpg","imgs/05.jpg","imgs/06.jpg",\
+             "imgs/07.jpg","imgs/08.jpg","imgs/09.jpg","imgs/10.jpg","imgs/11.jpg","imgs/12.jpg",\
+             "imgs/13.jpg","imgs/14.jpg","imgs/15.jpg"]
 
 for img_path in imgs_list:
 
@@ -34,8 +36,7 @@ for img_path in imgs_list:
     if img is None:
         sys.exit()
 
-    # img = cv2.resize(img, dsize=(300,300), interpolation=cv2.INTER_AREA)
-
+    # create blob object to detect faces
     blob1 = cv2.dnn.blobFromImage(img, scalefactor=1, size=(300, 300),\
                                   mean=(104, 177, 123))
     face_detect_net.setInput(blob1)
@@ -48,7 +49,7 @@ for img_path in imgs_list:
         confidence = face_detected[i, 2]
         if confidence < 0.5:
             break
-
+        
         x1 = int(face_detected[i, 3] * w)
         y1 = int(face_detected[i, 4] * h)
         x2 = int(face_detected[i, 5] * w)
@@ -56,18 +57,22 @@ for img_path in imgs_list:
 
         face = img[y1:y2+10, x1:x2].copy()
 
+        # create blob object to classify gender and age group
         blob2 = cv2.dnn.blobFromImage(face, scalefactor=1, size=(227,227), \
             mean=(78.4263377603, 87.7689143744, 114.895847746), \
             swapRB=False, crop=False)
 
+        # classify gender
         gender_classifier_net.setInput(blob2)
         gender_out = gender_classifier_net.forward()
         gender = gender_list[gender_out[0].argmax()]
 
+        # classify age
         age_classifier_net.setInput(blob2)
         age_out = age_classifier_net.forward()
         age = age_list[age_out[0].argmax()]
 
+        # draw rectangle around the face object
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0))
         label = f"{gender}, {age}"
         cv2.putText(img, label, (x1, y1-1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
